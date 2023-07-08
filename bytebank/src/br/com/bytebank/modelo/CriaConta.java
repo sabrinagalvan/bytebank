20f36745beba84fdc79876cfdd4a79a5be2e54c7
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-07-07 21:26:42 -0300

    indentando

--- bytebank/src/br/com/bytebank/modelo/CriaConta.java
+++ bytebank/src/br/com/bytebank/modelo/CriaConta.java
@@ -1,8 +1,9 @@
 package br.com.bytebank.modelo;
 
 public class CriaConta {
-    
+
     public static void main(String[] args) {
+        
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
