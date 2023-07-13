39df0406e7041784062667a42398699a6e9b451b
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-07-13 19:52:26 -0300

    indentando

--- bytebank/src/br/com/bytebank/modelo/CriaConta.java
+++ bytebank/src/br/com/bytebank/modelo/CriaConta.java
@@ -7,9 +7,10 @@ public class CriaConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
         System.out.println(primeiraConta.saldo);
-        
+
         primeiraConta.saldo += 100;
         System.out.println(primeiraConta.saldo);
+
         Conta segundaConta = new Conta();
         segundaConta.saldo = 300;
         System.out.println("primeira conta tem " + primeiraConta.saldo);
