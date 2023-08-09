91278a623e26a3736a2cc0be92575f2c82a49474
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-08-09 19:45:59 -0300

    indentando

--- bytebank/src/br/com/bytebank/test/TestaMetodo.java
+++ bytebank/src/br/com/bytebank/test/TestaMetodo.java
@@ -10,6 +10,7 @@ public class TestaMetodo {
         contaDoPaulo.saldo = 100;
         contaDoPaulo.deposita(50);
         System.out.println(contaDoPaulo.saldo);
+
         boolean conseguiuRetirar = contaDoPaulo.saca(20);
         System.out.println(contaDoPaulo.saldo);
         System.out.println(conseguiuRetirar);
