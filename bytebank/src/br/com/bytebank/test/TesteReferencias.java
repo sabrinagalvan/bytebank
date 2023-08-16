3871720260a94d3fd46ded8d07e319aa4cb948e4
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-08-16 19:48:24 -0300

    corrigindo System.out.println

--- bytebank/src/br/com/bytebank/test/TesteReferencias.java
+++ bytebank/src/br/com/bytebank/test/TesteReferencias.java
@@ -6,7 +6,7 @@ public class TesteReferencias {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 300;
-        System.out.println("saldo da primeira: " + primeiraConta.saldo);
+        System.out.println("Saldo da primeira: " + primeiraConta.saldo);
         Conta segundaConta = primeiraConta;
         System.out.println("saldo da segunda conta " + segundaConta.saldo);
         segundaConta.saldo += 100;
