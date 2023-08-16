a0251e7731b538445270dc23b40213174ab8a62d
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-08-16 19:51:03 -0300

    corrigindo System.out.println

--- bytebank/src/br/com/bytebank/test/TesteReferencias.java
+++ bytebank/src/br/com/bytebank/test/TesteReferencias.java
@@ -13,7 +13,7 @@ public class TesteReferencias {
         System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
         System.out.println(primeiraConta.saldo);
         if(primeiraConta == segundaConta) {
-            System.out.println("Sao a mesmissima conta.");
+            System.out.println("São a mesmissima conta.");
             System.out.println(primeiraConta);
             System.out.println(segundaConta);
         }
