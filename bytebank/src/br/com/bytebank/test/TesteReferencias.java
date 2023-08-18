bfbc82ceeacd84cff249c93361177fee47891f5c
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-08-18 19:50:38 -0300

    indentando

--- bytebank/src/br/com/bytebank/test/TesteReferencias.java
+++ bytebank/src/br/com/bytebank/test/TesteReferencias.java
@@ -9,6 +9,7 @@ public class TesteReferencias {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 300;
         System.out.println("Saldo da primeira: " + primeiraConta.saldo);
+
         Conta segundaConta = primeiraConta;
         System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
         segundaConta.saldo += 100;
