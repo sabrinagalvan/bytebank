e2c90e061c6c5b2377c4dff7247aa7ba16a2f478
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-09-11 19:50:19 -0300

    alterando nome

--- bytebank-composto/src/br/com/bytebank/composto/test/TestaBanco.java
+++ bytebank-composto/src/br/com/bytebank/composto/test/TestaBanco.java
@@ -7,7 +7,7 @@ public class TestaBanco {
 
     public static void main(String[] arg) {
 
-        Cliente paulo = new Cliente();
+        Cliente sabrina = new Cliente();
         paulo.nome = "Sabrina Galvan";
         paulo.cpf = "222.222.222-22";
         paulo.profissao = "programador";
