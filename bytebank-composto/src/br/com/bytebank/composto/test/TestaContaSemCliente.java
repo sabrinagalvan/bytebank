0df8192ce9ca638b6f058c7ae904c68d606c3709
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-09-13 19:55:11 -0300

    alterando nome

--- bytebank-composto/src/br/com/bytebank/composto/test/TestaContaSemCliente.java
+++ bytebank-composto/src/br/com/bytebank/composto/test/TestaContaSemCliente.java
@@ -5,11 +5,11 @@ import br.com.bytebank.composto.modelo.Conta;
 
 public class TestaContaSemCliente {
     public static void main(String[] args) {
-        Conta contaDaMarcela  = new Conta();
-        System.out.println(contaDaMarcela.getSaldo());
-        contaDaMarcela.titular = new Cliente();
-        System.out.println(contaDaMarcela.titular);
-        contaDaMarcela.titular.nome = "Marcela";
-        System.out.println(contaDaMarcela.titular.nome);
+        Conta contaDaBibi  = new Conta();
+        System.out.println(contaDaBibi.getSaldo());
+        contaDaBibi.titular = new Cliente();
+        System.out.println(contaDaBibi.titular);
+        contaDaBibi.titular.nome = "Marcela";
+        System.out.println(contaDaBibi.titular.nome);
     }
 }
