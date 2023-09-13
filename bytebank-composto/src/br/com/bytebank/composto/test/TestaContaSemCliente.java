059c7ee3f4862d17367f29856f8e20a0431edb02
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-09-13 19:55:52 -0300

    indentando

--- bytebank-composto/src/br/com/bytebank/composto/test/TestaContaSemCliente.java
+++ bytebank-composto/src/br/com/bytebank/composto/test/TestaContaSemCliente.java
@@ -4,6 +4,7 @@ import br.com.bytebank.composto.modelo.Cliente;
 import br.com.bytebank.composto.modelo.Conta;
 
 public class TestaContaSemCliente {
+
     public static void main(String[] args) {
         Conta contaDaBibi  = new Conta();
         System.out.println(contaDaBibi.getSaldo());
