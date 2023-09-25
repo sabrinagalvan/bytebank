ffa58be852fd86d41618696bc8e26ace3111f3c6
sabrinagalvan <sabrinagalvans2@gmail.com>
2023-09-25 19:23:50 -0300

    indentando

--- bytebank-composto/src/br/com/bytebank/composto/test/TesteSacaNegativo.java
+++ bytebank-composto/src/br/com/bytebank/composto/test/TesteSacaNegativo.java
@@ -9,6 +9,7 @@ public class TesteSacaNegativo {
         Conta conta = new Conta();
         conta.deposita(100);
         System.out.println(conta.saca(101));
+
         conta.saca(101);
         System.out.println(conta.getSaldo());
     }
