21edc278d2dbb4957490439e1cee1f26de4f3b50
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-02-21 20:01:49 -0300

    identando

--- bytebank-encapsulado/src/br/com/bytebank/encapsulado/test/TestaGetESet.java
+++ bytebank-encapsulado/src/br/com/bytebank/encapsulado/test/TestaGetESet.java
@@ -6,6 +6,7 @@ import br.com.bytebank.encapsulado.modelo.Conta;
 public class TestaGetESet {
 
     public static void main(String[] args) {
+
         Conta conta = new Conta(1337, 24226);
         System.out.println(conta.getNumero());
         Cliente paulo = new Cliente();
