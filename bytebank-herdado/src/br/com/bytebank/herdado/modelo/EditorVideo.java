e1662e35f132a470c0d5167638308c17f755e436
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-07-24 21:23:22 -0300

    identando

--- bytebank-herdado/src/br/com/bytebank/herdado/modelo/EditorVideo.java
+++ bytebank-herdado/src/br/com/bytebank/herdado/modelo/EditorVideo.java
@@ -2,8 +2,10 @@ package br.com.bytebank.herdado.modelo;
 
 //modelo.Gerente eh um modelo.Funcionario, modelo.Gerente herda da class modelo.Funcionario
 public class EditorVideo extends Funcionario {
+
     public double getBonificacao() {
         System.out.println("Chamando o método de bonificação do EDITOR DE VÍDEO");
         return 150;
     }
+
 }
