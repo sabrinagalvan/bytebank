8414a55786480e57c100938d455192a3b21951aa
sabrinagalvan <sabrinagalvans2@gmail.com>
2024-07-24 21:21:58 -0300

    identando

--- bytebank-herdado/src/br/com/bytebank/herdado/modelo/Designer.java
+++ bytebank-herdado/src/br/com/bytebank/herdado/modelo/Designer.java
@@ -2,8 +2,10 @@ package br.com.bytebank.herdado.modelo;
 
 //modelo.Gerente eh um modelo.Funcionario, modelo.Gerente herda da class modelo.Funcionario
 public class Designer extends Funcionario {
+
     public double getBonificacao() {
         System.out.println("Chamando o método de bonificação do DESIGNER");
         return 200;
     }
+
 }
